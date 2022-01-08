17a735fe326354d97a2b2067b82ac82568504a34
j-aguil <jagui105@calpoly.edu>
2022-01-07 23:48:37 -0800

    Created 'Form' file

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,27 +1,11 @@
 import React, {useState} from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 
 
 function MyApp(){
-    const [characters, setCharacters] = useState([
-    {
-        name: 'Charlie',
-        job: 'Janitor',
-    } ,
-    {
-        name: 'Mac',
-        job: 'Bouncer',
-    } ,
-    {
-        name: 'Dee',
-        job: 'Aspiring actress',
-    },
-    {
-        name: 'Dennis',
-        job: 'Bartender'
-      },
-    ]);
+    const [characters, setCharacters] = useState([]);
 
 function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
@@ -33,6 +17,7 @@ function removeOneCharacter (index) {
     return(
         <div className="container">
             <Table characterData = {characters} removeCharacter = {removeOneCharacter} />
+            <Form />
         </div>
     );
 }
